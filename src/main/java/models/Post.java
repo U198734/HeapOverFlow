758cd69b652e30a3e3f998a2383b5728d5e6e1d8
Telmo Linacisoro <telmolinacisoro@gmail.com>
2024-06-22 23:57:50 +0200

    Update Post.java

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,66 +1,85 @@
 package models;
+
+import java.io.Serializable;
 import java.sql.Timestamp;
 
+public class Post implements Serializable {
 
-// ACABAR DE IMPLEMENTAR GETTERS Y SETTERS. CLASE DE REFERENCIA EN EL TEMPLATE Tweet.java
+    private static final long serialVersionUID = 1L;
 
+    private int parentId;
+    private int postId;
+    private String username;
+    private String description;
+    private String url;
+    private String programmingLanguage;
+    private String professionalField;
+    private Timestamp postDateTime;
 
+    public Post() {
+    }
 
-public class Post implements java.io.Serializable {
+    public int getParentId() {
+        return parentId;
+    }
 
-	 private static final long serialVersionUID = 1L;
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
 
-	 private int parentId;
-	 private int postId; 
-	 private String username;
-	 private String description;
-	 private String url;
-	 private String programmingLanguage;
-	 private String professionalField;
-	 private Timestamp postDateTime;
+    public int getPostId() {
+        return postId;
+    }
 
-	 public Post() {
-	 }
-	 
-	 public Integer getParentId() {
-		 return this.parentId;
-	 }
-	 
-	 public void setParentId(Integer parentId) {
-		 this.parentId = parentId;
-	 }
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
 
-	 public Integer getPostId() {
-		 return this.postId;
-	 }
-	 
-	 public void setPostId(Integer postId) {
-		 this.postId = postId;
-	 }
-	 
-	 public String getUsername() {
-		 return this.username;
-	 }
-	 
-	 public void setUsername(String username) {
-		 this.username = username;
-	 }
+    public String getUsername() {
+        return username;
+    }
 
-	 public String getDescription() {
-		 return this.description;
-	 }
-	 
-	 public void setDescription(String description) {
-		 this.description = description;
-	 }
-	 
-	 
-	 public Timestamp getPostDateTime() {
-		 return this.postDateTime;
-	 }
-	 public void setPostDateTime(Timestamp postDateTime) {
-		 this.postDateTime = postDateTime;
-	 }
-	 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProfessionalField() {
+        return professionalField;
+    }
+
+    public void setProfessionalField(String professionalField) {
+        this.professionalField = professionalField;
+    }
+
+    public Timestamp getPostDateTime() {
+        return postDateTime;
+    }
+
+    public void setPostDateTime(Timestamp postDateTime) {
+        this.postDateTime = postDateTime;
+    }
 }
